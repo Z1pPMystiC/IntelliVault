1c43e5467f6a13b6d074cf64d94937ea1ad4a388
Z1pPMystiC <mishman3004@gmail.com>
2023-06-14 17:56:32 -0500

    Home and Navbar styling errors fixed, Added Log In page, Added Sign Up page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
+import LogIn from './components/pages/Login';
+import SignUp from './components/pages/Signup';
 
 import './App.css';
 
@@ -12,6 +14,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact Component={Home}/>
+        <Route path='/log-in' Component={LogIn} />
+        <Route path='/sign-up' Component={SignUp} />
       </Routes>
     </Router>
     </>
