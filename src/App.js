3b0ec99c49afa1c84c34a82092f3985a7ff5a0f7
Z1pPMystiC <mishman3004@gmail.com>
2023-06-14 17:37:59 -0500

    Home page complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
 
 import './App.css';
 
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' exact />
+        <Route path='/' exact Component={Home}/>
       </Routes>
     </Router>
     </>
---
+++ src/components/HeroSection.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from './Button';
+import './HeroSection.css';
+import'../App.css';
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src='/videos/tech-background.mp4' autoPlay loop muted />
+            <h1>The Password Manager of the Future</h1>
+            <p>Now With the Power of Artificial Intelligence</p>
+            <div className="hero-btns">
+                <Button className='btns'buttonStyle='btn--outline' 
+                buttonSize='btn--large'>Get Started</Button>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
